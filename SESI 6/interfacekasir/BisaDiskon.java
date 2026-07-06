6007e73d02d5f5c4d2ecd90ea789c7055949a5a4
nimuruu <naufalmeitsamar@gmail.com>
2026-07-06 18:11:44 +0700

    Delete SESI 6 directory

--- SESI 6/interfacekasir/BisaDiskon.java
+++
@@ -1,7 +0,0 @@
-package interfacekasir;
-
-public interface BisaDiskon {
-    int PERSEN_DISKON = 10;
-    
-    int hitungDiskon();
-}
--- SESI 6/interfacekasir/BisaStok.java
+++
@@ -1,8 +0,0 @@
-package interfacekasir;
-
-public interface BisaStok {
-    int STOK_MINIMUM = 5;
-
-    boolean cekStok();
-    void kurangiStok();
-}
